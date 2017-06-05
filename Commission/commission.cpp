952440eb3274d9dc01f3a68518ebe1fe5b86da73
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-06-05 23:43:38 +0800

    Add commission function

---
+++ Commission/commission.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+string commission(int locks, int stocks, int barrels){
+    if( locks < 1 || locks > 70
+    || stocks < 1 || stocks > 80
+    || barrels < 1 || barrels > 90 )
+        return "Out of range value";
+    float sales = locks * 45 + stocks * 30 + barrels * 25;
+    if( sales > 1800 ){
+        return to_string(1000 * 0.1 + 800 * 0.15 + (sales - 1800) * 0.2);
+    }else if( sales > 1000 ){
+        return to_string(1000 * 0.1 + (sales - 1000) * 0.15);
+    }else{
+        return to_string(sales * 0.1);
+    }
+}
