80b40e3a56611dc7ab8da6fc1f1fec9fd2f9f636
actkz <actkz.cs02@g2.nctu.edu.tw>
2017-06-07 08:50:03 +0000

    Fix klee bugs with c++

--- NextDate/nextdate_klee.c
+++ NextDate/nextdate_klee.c
@@ -1,32 +1,31 @@
 #include "klee/klee.h"
-#include <iostream>
-#include <string>
-using namespace std;
-string NextDate( int year, int month, int day){
-    string year_s = to_string(year), month_s = to_string(month), day_s = to_string(day), output;
+#include <stdio.h>
+#include <string.h>
+int NextDate( int year, int month, int day){
+    int output;
     int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     if( year < 1900 || year > 2025 || month < 1 || month > 12 )
-        return "Out of range value";
+        return 1;
     if( (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
         days[2] += 1;
     }
     if( day < 1 || day > 31)
-        return "Out of range value";
+        return 1;
     else if( day > days[month] )
-        return "Invalid date";
+        return 2;
     if( month == 12 && day == 31 )
-        output = to_string(year+1)+"/1/1";
+        output = (year+1)*10000+101;
     else if(day == days[month])
-        output = year_s+"/"+to_string(month+1)+"/1";
+        output = year*10000+(month+1)*100+1;
     else
-        output = year_s+"/"+month_s+"/"+to_string(day+1);
+        output = year*10000+month*100+day+1;
     return output;
 }
-int main(){
+int main(int argc, char** argv){
     int year, month, day;
     klee_make_symbolic(&year, sizeof(year), "year");
     klee_make_symbolic(&month, sizeof(month), "month");
     klee_make_symbolic(&day, sizeof(day), "day");
-    printf("%s\n",NextDate(year, month, day).data());
+    printf("%d\n", NextDate(year, month, day));
     return 0;
 }
